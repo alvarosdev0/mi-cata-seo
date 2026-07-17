3a3019f635e18deb37a3224ad6543e7ea2a1046a
Álvaro Sandrea <120410797+alvarosdev0@users.noreply.github.com>
2026-07-17 09:42:32 -0400

    fix: bodyParser config en AuthModule.forRoot

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,7 +6,14 @@ import { auth } from './auth';
 
 @Module({
   imports: [
-    AuthModule.forRoot({ auth }),
+    AuthModule.forRoot({
+      auth,
+      bodyParser: {
+        json: { limit: "2mb" },
+        urlencoded: { limit: "2mb", extended: true },
+        rawBody: true,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
