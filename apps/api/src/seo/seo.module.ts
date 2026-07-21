239471ff95e610e8cb52c290de2e84fadfcad125
Álvaro Sandrea <120410797+alvarosdev0@users.noreply.github.com>
2026-07-21 10:03:01 -0400

    Revert "Merge pull request #5 from alvarosdev0/CATA-5-seo-validator"
    This reverts commit 5af483eab4ea926a7b18541927e2e4e9e1118fbc, reversing
    changes made to 60797c637aa29d227f4d261bd058e7a58347fff8.

--- apps/api/src/seo/seo.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SeoService } from './seo.service';
-
-@Module({
-  providers: [SeoService],
-  exports: [SeoService],
-})
-export class SeoModule {}
